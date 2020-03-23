f3c1e86028a7bb0074bc5ca86d1ce8b4cf8bdbb8
Adriana Martins <desaoadr@students.zhaw.ch>
2020-03-23 21:41:30 +0100

    creates final rows with the Total

--- src/main/java/ch/zhaw/unicalc/pdfGenerator/Service/PDFService.java
+++ src/main/java/ch/zhaw/unicalc/pdfGenerator/Service/PDFService.java
@@ -20,6 +20,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PDFService {
@@ -45,7 +47,8 @@ public class PDFService {
 
             Table table = new Table(UnitValue.createPercentArray(width)).useAllAvailableWidth();
             createHeader(table);
-            createContent(table, offerRequest);
+            Map totalMap = createContent(table, offerRequest);
+            createTotal(table, totalMap);
 
 
             doc.add(table);
@@ -65,6 +68,7 @@ public class PDFService {
 
         return null;
     }
+
 
     /**
      *  Creates Header for the Table
@@ -86,8 +90,9 @@ public class PDFService {
      * Creates the content from the JSON
      * @param table
      */
-    private void createContent(Table table, OfferRequest offerRequest) {
-        double i = 1;
+    private Map<Integer, Double> createContent(Table table, OfferRequest offerRequest) {
+        int i = 1;
+        double finalPrice = 0.0;
         for(SegmentRequest segment: offerRequest.getSegments()) {
             Cell cell1 = new Cell(1,1)
                     .add(new Paragraph(Integer.toString((int) i)))
@@ -102,7 +107,7 @@ public class PDFService {
                     .setTextAlignment(TextAlignment.LEFT);
             table.addCell(cell2);
             boolean even = false;
-            double j = 0.01;
+            double j = 0.1;
 
             for(ArticleRequest article: segment.getArticles()) {
                 Cell nr = new Cell().add(new Paragraph(j + i + "")).setFontSize(11);
@@ -122,16 +127,17 @@ public class PDFService {
                     totald -= dis;
                 }
                 totald =  Math.floor(totald * 100) / 100;
+                finalPrice += totald;
                 Cell total = new Cell().add(new Paragraph(totald + ".-")).setFontSize(11);
 
                 if(even) {
-                    nr.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    name.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    articleNr.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    amount.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    price.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    discount.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
-                    total.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.makeLighter(DeviceGray.GRAY)));
+                    nr.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    name.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    articleNr.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    amount.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    price.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    discount.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
+                    total.setBackgroundColor(DeviceGray.makeLighter(DeviceGray.GRAY));
                 }
 
                 table.addCell(nr);
@@ -142,11 +148,83 @@ public class PDFService {
                 table.addCell(discount);
                 table.addCell(total);
                 even = !even;
-                j = j + 0.01;
+                j = j + 0.1;
             }
 
             i++;
         }
+        Map<Integer, Double> map = new HashMap<>();
+        map.put(i, finalPrice);
+        return map;
+
+    }
+
+    private void createTotal(Table table, Map<Integer, Double> totalMap) {
+        totalMap.forEach((k,v) -> {
+            Cell cell1 = new Cell(1,1)
+                    .add(new Paragraph(k + ""))
+                    .setFontSize(12)
+                    .setBackgroundColor(DeviceGray.GRAY)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell1);
+            Cell cell2 = new Cell(1,6)
+                    .add(new Paragraph("Total"))
+                    .setFontSize(12)
+                    .setBackgroundColor(DeviceGray.GRAY)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell2);
+            Cell cell3 = new Cell(1,1)
+                    .add(new Paragraph(0.1 + k + ""))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell3);
+            Cell cell4 = new Cell(1,4)
+                    .add(new Paragraph("Total Brutto"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell4);
+            Cell cell5 = new Cell(1,1)
+                    .add(new Paragraph("??%"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell5);
+            Cell cell6 = new Cell(1,1)
+                    .add(new Paragraph(Math.floor(v * 100) / 100 + ".-"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell6);
+            Cell cell7 = new Cell(1,1)
+                    .add(new Paragraph(0.2 + k + ""))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell7);
+            Cell cell8 = new Cell(1,6)
+                    .add(new Paragraph("Zusatzrabatt bei Abholung"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell8);
+            Cell cell9 = new Cell(1,1)
+                    .add(new Paragraph(0.3 + k + ""))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell9);
+            Cell cell10 = new Cell(1,4)
+                    .add(new Paragraph("MwST."))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell10);
+            Cell cell11 = new Cell(1,1)
+                    .add(new Paragraph("7.7%"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell11);
+
+            Cell cell12 = new Cell(1,1)
+                    .add(new Paragraph("??"))
+                    .setFontSize(11)
+                    .setTextAlignment(TextAlignment.LEFT);
+            table.addCell(cell12);
+        });
 
     }
 
