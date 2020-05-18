1301e95702e65d6b1d959d25e4f8be3bb28b9bf4
Adriana Martins <desaoadr@students.zhaw.ch>
2020-05-18 17:25:47 +0200

    fixed import error

--- src/test/java/ch/zhaw/unicalc/pdfGenerator/PDFControllerTest.java
+++ src/test/java/ch/zhaw/unicalc/pdfGenerator/PDFControllerTest.java
@@ -3,7 +3,6 @@ package ch.zhaw.unicalc.pdfGenerator;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,19 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.NestedServletException;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
