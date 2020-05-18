bcfc0b2925278f566172dec00747e53c23f42887
Adriana Martins <desaoadr@students.zhaw.ch>
2020-05-18 20:22:39 +0200

    fixed some generating Problems

--- src/test/java/ch/zhaw/unicalc/pdfGenerator/PDFControllerTest.java
+++ src/test/java/ch/zhaw/unicalc/pdfGenerator/PDFControllerTest.java
@@ -1,9 +1,12 @@
 package ch.zhaw.unicalc.pdfGenerator;
 
+import ch.zhaw.unicalc.pdfGenerator.Model.Transfer.OfferRequest;
+import ch.zhaw.unicalc.pdfGenerator.Service.OfferInvoicePdf;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,10 +15,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.NestedServletException;
 
 import java.io.IOException;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +33,12 @@ public class PDFControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    private OfferInvoicePdf offerInvoicePdf;
+
+    @Mock
+    private OfferRequest offerRequest;
+
     @BeforeAll
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -37,6 +46,7 @@ public class PDFControllerTest {
 
     @Test
     public void testConnectionOffer_ok() throws Exception {
+        when(offerInvoicePdf.generatePDF(true, offerRequest)).thenReturn(new byte[]{});
         this.mockMvc.perform(post("/toPdf/offer")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(convertJSON("/controller/offer.json")))
@@ -45,19 +55,12 @@ public class PDFControllerTest {
 
     @Test
     public void testConnectionInvoice_ok() throws Exception {
+        when(offerInvoicePdf.generatePDF(false, offerRequest)).thenReturn(new byte[]{});
         this.mockMvc.perform(post("/toPdf/invoice")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(convertJSON("/controller/invoice.json")))
                 .andExpect(status().is2xxSuccessful());
     }
-
-    @Test(expected = NestedServletException.class)
-    public void invalidLogoFileTest() throws Exception {
-        this.mockMvc.perform(post("/toPdf/offer")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(convertJSON("/controller/invoiceInvalidLogo.json")));
-    }
-
 
     private String convertJSON(String json) throws IOException {
         return IOUtils.toString(getClass().getResourceAsStream(json), "UTF-8");
